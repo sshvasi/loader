0b8b4630a2606acf0b182df77b5cfd4e537ff6b6
Alexander Vasiliev <asvasi25@gmail.com>
2022-09-04 21:59:56 +0300

    feat: add loading and saving logic

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import os from 'os';
 import path from 'path';
 import fs from 'fs/promises';
 import nock from 'nock';
-import pageLoader from '../src/index.js';
+import pageLoader from '../index.js';
 
 nock.disableNetConnect();
 
@@ -14,18 +14,16 @@ const pageUrl = new URL(pagePath, baseUrl);
 const fileName = 'ru-hexlet-io-courses.html';
 const fileContent = 'Каталог курсов по программированию на Хекслете';
 
-beforeEach( async () => {
+beforeEach(async () => {
   tmpDirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
 test('pageLoad() download page and write its content in file', async () => {
-  const scope = nock(baseUrl).get(pagePath).reply(200, fileContent);
-
-  expect(scope.isDone()).toBe(true);
+  nock(baseUrl).get(pagePath).reply(200, fileContent);
 
   const actualFileName = await pageLoader(pageUrl, tmpDirPath);
   expect(actualFileName).toBe(fileName);
 
-  const actualFileContent = await fs.readFile(path.join(tmpDirPath, fileName));
+  const actualFileContent = await fs.readFile(path.resolve(tmpDirPath, fileName), 'utf-8');
   expect(actualFileContent).toBe(fileContent);
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-const pageLoader = () => {};
+import path from 'path';
+import fs from 'fs/promises';
+import axios from 'axios';
+
+const urlToFilename = (url, postfix) => {
+  const { hostname, pathname } = url;
+  const filename = `${hostname}${pathname}`
+    .match(/\w*/gi)
+    .filter((w) => w)
+    .join('-');
+
+  return `${filename}${postfix}`;
+};
+
+const writeFile = (dirpath, filename, content) => {
+  const filepath = path.resolve(dirpath, filename);
+  fs.writeFile(filepath, content);
+};
+
+const pageLoader = (url, dirpath = process.cwd()) => axios.get(url.toString())
+  .then(({ data }) => {
+    const filename = urlToFilename(url, '.html');
+    writeFile(dirpath, filename, data);
+
+    return filename;
+  })
+  .then((filename) => filename);
 
 export default pageLoader;
