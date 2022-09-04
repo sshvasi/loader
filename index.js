080cc420df3f404286ff876660049e9169c99814
Alexander Vasiliev <asvasi25@gmail.com>
2022-09-05 01:04:10 +0300

    refactor: prettify fns

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import pageLoader from './src/index.js';
+import loadPage from './src/index.js';
 
-export default pageLoader;
+export default loadPage;
--- src/index.js
+++ src/index.js
@@ -2,28 +2,26 @@ import path from 'path';
 import fs from 'fs/promises';
 import axios from 'axios';
 
-const urlToFilename = (url, postfix) => {
+const urlToFilename = (url, postfix = '.html') => {
   const { hostname, pathname } = url;
-  const filename = `${hostname}${pathname}`
-    .match(/\w*/gi)
-    .filter((w) => w)
-    .join('-');
+  const filename = `${hostname}${pathname}`.replace(/\W/g, '-');
 
   return `${filename}${postfix}`;
 };
 
 const writeFile = (dirpath, filename, content) => {
   const filepath = path.resolve(dirpath, filename);
-  fs.writeFile(filepath, content);
+
+  return fs.writeFile(filepath, content);
 };
 
-const pageLoader = (url, dirpath = process.cwd()) => axios.get(url.toString())
-  .then(({ data }) => {
-    const filename = urlToFilename(url, '.html');
-    writeFile(dirpath, filename, data);
+const loadPage = (url, dirpath = process.cwd()) => {
+  const { href } = url;
+  const filename = urlToFilename(url);
 
-    return filename;
-  })
-  .then((filename) => filename);
+  return axios.get(href)
+    .then(({ data }) => writeFile(dirpath, filename, data))
+    .then(() => filename);
+};
 
-export default pageLoader;
+export default loadPage;
