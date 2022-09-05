49027b4395e6cf372ee95adc80ed5c5ad2e15fa9
Alexander Vasiliev <asvasi25@gmail.com>
2022-09-05 15:25:36 +0300

    feat: add cli

--- bin/pageloader.js
+++ bin/pageloader.js
@@ -1,1 +1,22 @@
 #!/usr/bin/env node
+
+import { Command } from 'commander';
+import pageLoader from '../index.js';
+
+const program = new Command();
+
+const VERSION = '1.0.0';
+const DESCRIPTION = 'Loads page and saves its content in dir.';
+
+program
+  .name('page-loader')
+  .description(DESCRIPTION)
+  .version(VERSION)
+  .arguments('<url>')
+  .option('-o, --output [dir]', 'output dir', process.cwd())
+  .action((url) => {
+    const { output } = program.opts();
+    pageLoader(url, output);
+  });
+
+program.parse(process.argv);
