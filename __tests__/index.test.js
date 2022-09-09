d279fafe87459a3563606c7364e2666c8fb71d03
Alexander Vasiliev <asvasi25@gmail.com>
2022-09-09 04:00:30 +0300

    refactor: reorganize tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,7 @@ import os from 'os';
 import path from 'path';
 import fs from 'fs/promises';
 import nock from 'nock';
-import { pageURLToName } from '../src/index.js';
+import { urlToDirname, urlToFilename } from '../src/utils.js';
 import pageLoader from '../index.js';
 
 const __filename = url.fileURLToPath(import.meta.url);
@@ -14,62 +14,64 @@ const readFixtureFile = (dirname, filename) => fs.readFile(getFixturePath(dirnam
 
 const BASE_URL = 'https://ru.hexlet.io';
 const PAGE_PATH = '/courses';
-const PAGE_PATH_BEFORE = '/courses/before';
-const IMG_PATH = '/assets/professions/nodejs';
+const IMG_PATH = '/assets/professions/nodejs.png';
 
-const pageURL = new URL(PAGE_PATH, BASE_URL);
-const pageURLBefore = new URL(PAGE_PATH_BEFORE, BASE_URL);
-const imgURL = new URL(IMG_PATH, BASE_URL);
+const pageUrl = new URL(PAGE_PATH, BASE_URL);
+const imgUrl = new URL(IMG_PATH, BASE_URL);
 
-const filename = pageURLToName(pageURL);
-const filenameBefore = pageURLToName(pageURLBefore);
-const imgname = pageURLToName(imgURL, '.png');
-const dirname = pageURLToName(pageURL, '_files');
+const pageFilename = urlToFilename(pageUrl);
+const imgFilename = urlToFilename(imgUrl);
+const assetsDirname = urlToDirname(pageUrl);
 
 let tmpDirpath;
-let fileContent;
-let fileContentBefore;
-let imgContent;
+let expectedFileContent;
+let expectedImgContent;
+let responseFileContent;
 
 nock.disableNetConnect();
 
+const scope = nock(BASE_URL).persist();
+
 beforeAll(async () => {
-  fileContent = await readFixtureFile('.', filename);
-  fileContentBefore = await readFixtureFile('.', filenameBefore);
-  imgContent = await readFixtureFile(dirname, imgname);
+  expectedFileContent = await readFixtureFile('expected', pageFilename);
+  expectedImgContent = await readFixtureFile(
+    path.join('expected', assetsDirname),
+    imgFilename
+  );
+  responseFileContent = await readFixtureFile('.', pageFilename);
+
+  scope.get(PAGE_PATH).reply(200, responseFileContent);
+  scope.get(IMG_PATH).reply(200, expectedImgContent);
 });
 
 beforeEach(async () => {
   tmpDirpath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-
-  nock(BASE_URL).get(PAGE_PATH_BEFORE).reply(200, fileContentBefore);
-  nock(BASE_URL).get(PAGE_PATH).reply(200, fileContent);
 });
 
 describe('pageLoader', () => {
   it('should create html file with correct name', async () => {
-    const actualFilename = await pageLoader(pageURL, tmpDirpath);
-    expect(actualFilename).toBe(filename);
+    const actualFilename = await pageLoader(pageUrl, tmpDirpath);
+    expect(actualFilename).toBe(pageFilename);
   });
 
-  it('should download page and save it in output dir', async () => {
-    await pageLoader(pageURL, tmpDirpath);
+  it('should load page, make img src paths relative and save page in output dir', async () => {
+    await pageLoader(pageUrl, tmpDirpath);
 
-    const actualFileContent = await fs.readFile(path.join(tmpDirpath, filename), 'utf-8');
-    expect(actualFileContent).toBe(fileContent);
+    const actualFileContent = await fs.readFile(
+      path.join(tmpDirpath, pageFilename),
+      'utf-8'
+    );
+    console.log(actualFileContent);
+    expect(actualFileContent).toBe(expectedFileContent);
   });
 
-  it('should download page images and save they in <output_dir>_files dir', async () => {
-    await pageLoader(pageURL, tmpDirpath);
+  it('should load page images and <output_dir>_files dir', async () => {
+    await pageLoader(pageUrl, tmpDirpath);
 
-    const actualImageContent = await fs.readFile(path.join(tmpDirpath, dirname, imgname), 'utf-8');
-    expect(actualImageContent).toBe(imgContent);
-  });
-
-  it('should change link paths for resources after loading page', async () => {
-    await pageLoader(pageURLBefore, tmpDirpath);
-
-    const actualFileContent = await fs.readFile(path.join(tmpDirpath, filename), 'utf-8');
-    expect(actualFileContent).toBe(fileContent);
+    const actualImageContent = await fs.readFile(
+      path.join(tmpDirpath, assetsDirname, imgFilename),
+      'utf-8'
+    );
+    expect(actualImageContent).toBe(expectedImgContent);
   });
 });
